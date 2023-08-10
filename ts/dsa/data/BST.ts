8dc4707ab5356fe9e732e5d508b59d24117f8879
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2023-08-10 13:55:19 -0700

    clean up BST.ts, add comments

--- ts/dsa/data/BST.ts
+++ ts/dsa/data/BST.ts
@@ -11,18 +11,30 @@ class BTNode<T> {
 }
 
 
+/**
+ * TODO: generator that yields in order
+ */
+
 export class BST<T> {
 	head: BTNode<T> | null;
     comp: (a: T, b: T) => number;
 
 
+    /**
+     * @param comp comparison function, returns negative when `a` comes before `b`
+     * 0, when equal, and positive when `b` comes before `a`.
+     */
 	constructor(comp: (a: T, b: T) => number = (a, b) => a > b ? -1 : a < b ? 1 : 0 ) {
 		this.head = null;
         this.comp = comp;
 	}
 
     
-    private *bfsIterator() {
+    /**
+     * Recursive breadth-first generator, yields each node
+     * @returns generator
+     */
+    private *bfsNodes() {
         function *traverse(node: BTNode<T> | null, done: boolean = false): Generator<BTNode<T>> {
             if (!node || done) return;
 
@@ -43,43 +55,50 @@ export class BST<T> {
         yield *traverse(this.head);
     }
 
-
-    forEachBFS(callback: (value: T) => unknown) {
-        for (const node of this.bfsIterator()) {
-            if (callback(node.value) === -1)
-                break;
-        }
+    /**
+     * Generator that yields each value in breadth-first order
+     * @returns generator
+     */
+    public *bfsValues() {
+        for (const node of this.bfsNodes())
+            yield node.value;
     }
 	
-
+    /**
+     * Inserts a value into the tree
+     * @param value 
+     * @returns this tree for chaining
+     */
 	emplace(value: T) {
+
+        // no head? create it with the new value
 		if (!this.head) {
 			this.head = new BTNode(value);
 			return this;
 		}
 		
+        // search tree for spot to insert
 		let cur = this.head;
 		while (true) {
+            // duplicate values not allowed
 			if (this.comp(cur.value, value) === 0) {
-				// don't do anything if duplicate
-				
-				return this;
+				throw new Error("BST: illegal duplicate value: " + value);
 			}
 			
 			
 			if (this.comp(cur.value, value) < 0) {
+                // value is less than current 
 				if (cur.left) {
 					cur = cur.left;
-					continue;
-				} else {
+				} else { // found empty position to insert
 					cur.left = new BTNode(value);
 					return this;
 				}
 			} else {
+                // value is more than current
 				if (cur.right) {
 					cur = cur.right;
-					continue;
-				} else {
+				} else { // found empty position to insert
 					cur.right = new BTNode(value);
 					return this;
 				}
@@ -87,25 +106,27 @@ export class BST<T> {
 		}
 	}
 
-    
-	has(value: T) {
+
+    /**
+     * Checks whether tree contains a value
+     * @param value - value to check
+     */
+	has(value: T): boolean {
 		if (!this.head) return false;
 		
 		let cur = this.head;
 		while (true) {
 			if (cur.value === value) return true;
 			
-			if (this.comp(cur.value, value) < 0 && cur.left) {
-				cur = cur.left;
-				continue;
-			}
-			
-			if (this.comp(cur.value, value) > 0 && cur.right) {
-				cur = cur.right;
-				continue;
-			}
-			
-			return false;
+			if (this.comp(cur.value, value)) {
+                if (cur.left)
+				    cur = cur.left;
+			} else {
+                if (cur.right)
+                    cur = cur.right;
+            }
+
+            return false;
 		}
 	}
 }
--- ts/tests/BST.test.ts
+++ ts/tests/BST.test.ts
@@ -1,7 +1,9 @@
 import { BST } from "../dsa/data/BST";
 
 describe("BST", () => {
-    const t = new BST<number>();
+    let t: BST<number>;
+
+    beforeEach(() => t = new BST<number>());
 
     test("BST insert", () => {
         t.emplace(5);
@@ -17,10 +19,7 @@ describe("BST", () => {
         t.emplace(4);
 
         const expected: number[] = [5, 4, 10, 7, 8];
-        const actual: number[] = [];
-        t.forEachBFS((val) => {
-            actual.push(val);
-        });
+        const actual: number[] = Array.from(t.bfsValues());
 
         expect(actual).toEqual(expected);
     });
